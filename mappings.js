5f2ee74b152d280475e2cb6c0232d229cd3b545a
PepeYT <106378846+PepeOnGithub@users.noreply.github.com>
2024-08-13 13:02:05 +0200

    Update mappings.js

--- mappings.js
+++ mappings.js
@@ -1,25 +1,33 @@
 export const fileMappings = {
-    // Logs
     "acacia_log": "log_acacia",
     "birch_log": "log_birch",
     "dark_oak_log": "log_big_oak",
     "jungle_log": "log_jungle",
     "oak_log": "log_oak",
     "spruce_log": "log_spruce",
-
-    // Stems (Nether logs)
     "warped_stem": "log_warped",
     "crimson_stem": "log_crimson",
-
-    // Dirt paths
+    "acacia_planks": "planks_acacia",
+    "birch_planks": "planks_birch",
+    "dark_oak_planks": "planks_big_oak",
+    "jungle_planks": "planks_jungle",
+    "oak_planks": "planks_oak",
+    "spruce_planks": "planks_spruce",
+    "wooden_axe": "wood_axe",
+    "wooden_pickaxe": "wood_pickaxe",
+    "wooden_shovel": "wood_shovel",
+    "wooden_sword": "wood_sword",
+    "wooden_hoe": "wood_hoe",
+    "golden_axe": "gold_axe",
+    "golden_pickaxe": "gold_pickaxe",
+    "golden_shovel": "gold_shovel",
+    "golden_sword": "gold_sword",
+    "golden_hoe": "gold_hoe",
+    "gold_block": "block_gold",
     "dirt_path": "grass_path",
-
-    // Other blocks
     "mushroom_block": "huge_mushroom_block",
     "stone_slab_top": "double_stone_slab_top",
     "grass_side": "grass_side_carried",
-
-    // Items
     "fishing_rod_uncast": "fishing_rod",
     "fishing_rod_cast": "fishing_rod_cast",
     "nether_star": "star",
@@ -29,8 +37,6 @@ export const fileMappings = {
     "leather_chestplate": "leather_tunic",
     "leather_leggings": "leather_pants",
     "leather_boots": "leather_boots",
-
-    // Entities
     "zombie_pigman": "zombified_piglin",
     "wolf": "dog",
     "snowman": "snow_golem",
