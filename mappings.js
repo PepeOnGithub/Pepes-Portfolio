975f6b9bfe731b52d101222b4d6eee64567c2206
PepeYT <106378846+PepeOnGithub@users.noreply.github.com>
2024-08-13 12:59:37 +0200

    Update

---
+++ mappings.js
@@ -0,0 +1,40 @@
+export const fileMappings = {
+    // Logs
+    "acacia_log": "log_acacia",
+    "birch_log": "log_birch",
+    "dark_oak_log": "log_big_oak",
+    "jungle_log": "log_jungle",
+    "oak_log": "log_oak",
+    "spruce_log": "log_spruce",
+
+    // Stems (Nether logs)
+    "warped_stem": "log_warped",
+    "crimson_stem": "log_crimson",
+
+    // Dirt paths
+    "dirt_path": "grass_path",
+
+    // Other blocks
+    "mushroom_block": "huge_mushroom_block",
+    "stone_slab_top": "double_stone_slab_top",
+    "grass_side": "grass_side_carried",
+
+    // Items
+    "fishing_rod_uncast": "fishing_rod",
+    "fishing_rod_cast": "fishing_rod_cast",
+    "nether_star": "star",
+    "saddle": "horse_saddle",
+    "fire_charge": "fireball",
+    "leather_helmet": "leather_cap",
+    "leather_chestplate": "leather_tunic",
+    "leather_leggings": "leather_pants",
+    "leather_boots": "leather_boots",
+
+    // Entities
+    "zombie_pigman": "zombified_piglin",
+    "wolf": "dog",
+    "snowman": "snow_golem",
+    "wither_skeleton": "wither_skeleton",
+    "elder_guardian": "guardian_elder",
+    "horse/armor": "horse/armor/horse_armor"
+};
