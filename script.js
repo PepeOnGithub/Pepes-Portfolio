28addfdb2344e0ab8b92c44b5649c1485aeba354
PepeOnGithub <106378846+PepeOnGithub@users.noreply.github.com>
2024-10-12 07:32:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+export const fileMappings = {
+    "acacia_log": "log_acacia",
+    "birch_log": "log_birch",
+    "dark_oak_log": "log_big_oak",
+    "jungle_log": "log_jungle",
+    "oak_log": "log_oak",
+    "spruce_log": "log_spruce",
+    "warped_stem": "log_warped",
+    "crimson_stem": "log_crimson",
+    "acacia_planks": "planks_acacia",
+    "birch_planks": "planks_birch",
+    "dark_oak_planks": "planks_big_oak",
+    "jungle_planks": "planks_jungle",
+    "oak_planks": "planks_oak",
+    "spruce_planks": "planks_spruce",
+    "wooden_axe": "wood_axe",
+    "wooden_pickaxe": "wood_pickaxe",
+    "wooden_shovel": "wood_shovel",
+    "wooden_sword": "wood_sword",
+    "wooden_hoe": "wood_hoe",
+    "golden_axe": "gold_axe",
+    "golden_pickaxe": "gold_pickaxe",
+    "golden_shovel": "gold_shovel",
+    "golden_sword": "gold_sword",
+    "golden_hoe": "gold_hoe",
+    "gold_block": "block_gold",
+    "dirt_path": "grass_path",
+    "mushroom_block": "huge_mushroom_block",
+    "stone_slab_top": "double_stone_slab_top",
+    "grass_side": "grass_side_carried",
+    "fishing_rod_uncast": "fishing_rod",
+    "fishing_rod_cast": "fishing_rod_cast",
+    "nether_star": "star",
+    "saddle": "horse_saddle",
+    "fire_charge": "fireball",
+    "leather_helmet": "leather_cap",
+    "leather_chestplate": "leather_tunic",
+    "leather_leggings": "leather_pants",
+    "leather_boots": "leather_boots",
+    "zombie_pigman": "zombified_piglin",
+    "wolf": "dog",
+    "snowman": "snow_golem",
+    "wither_skeleton": "wither_skeleton",
+    "elder_guardian": "guardian_elder",
+    "horse/armor": "horse/armor/horse_armor"
+};
+
 function convertPack() {
     const fileInput = document.getElementById('javaPack');
     const file = fileInput.files[0];
@@ -32,6 +79,11 @@ function convertPack() {
                     newFileName = fileName.replace('assets/minecraft/textures/mob_effect/', 'textures/ui/mob_effect/');
                 } else if (fileName.startsWith('assets/minecraft/textures/map_icon/')) {
                     newFileName = fileName.replace('assets/minecraft/textures/map_icon/', 'textures/map/map_icons/');
+                }
+
+                if (fileMappings.hasOwnProperty(fileName.split('/').pop().split('.')[0])) {
+                    const fileBaseName = fileName.split('/').pop().split('.')[0];
+                    newFileName = fileName.replace(fileBaseName, fileMappings[fileBaseName]);
                 }
 
                 if (fileName === 'pack.png') {
